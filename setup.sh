9980d47c6335517c552ccca97d7470e60c1bbb10
thomaska <thomas.kaliakos@OVO2279mb.local>
2017-06-01 12:05:15 +0100

    Add homebrew, GCP, kubectl and Confluent platform

--- setup.sh
+++ setup.sh
@@ -2,4 +2,18 @@
 cp ./.bash_rc ./.gitconfig ~/
 
 ## Install homebrew
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## Install latest Gcp 
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz
+gunzip -c google-cloud-sdk.tar.gz | tar xopf -
+./google-cloud-sdk/install.sh --quiet
+
+## Install kubectl (requires Gcp)
+gcloud components install kubectl --quiet
+
+## Install Confluent Platform 3.2.1
+wget http://packages.confluent.io/archive/3.2/confluent-oss-3.2.1-2.11.tar.gz
+gunzip -c confluent-oss-3.2.1-2.11.tar.gz | tar xopf -
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PATH=$PATH:$DIR/confluent-3.2.1/bin
