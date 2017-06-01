2dcef946d09faffd8edee25426b9a291b4a15a2e
Thomas.Kaliakos <thomas.kaliakos@OVO2279mb.local>
2017-06-01 10:15:07 +0100

    Add .gitconfig .bash_rc

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mac_setup
 Install basic development tools in a Mac
+
+## Run
+Just run `./setup.sh` from the command line
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ./.bash_rc ./.gitconfig ~/
+
+## Install homebrew
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
